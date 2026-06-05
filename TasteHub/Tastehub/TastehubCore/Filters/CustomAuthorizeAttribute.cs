f00641f681092790209b188cfa4244ad72e389c6
Anmolkrch <50050462+Anmolkrch@users.noreply.github.com>
2026-06-05 06:51:55 +0530

    Addeed .NEt core

---
+++ TasteHub/Tastehub/TastehubCore/Filters/CustomAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TastehubCore.Filters
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (UserAuthenticate.IsAuthenticated)
+            {
+                return; // Allow request
+            }
+            else
+            {
+                context.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(
+                        new
+                        {
+                            controller = "Account",
+                            action = "LogOff"
+                        })
+                );
+            }
+        }
+    }
+}
